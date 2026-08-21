7cc09e8b9f9f2aaf281bb745207ec7349b9a2532
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2026-08-21 11:02:34 +0200

    Not sure why it was oras before but fixed container download now

--- lib/Containers.groovy
+++ lib/Containers.groovy
@@ -3,7 +3,7 @@ class Containers {
   public static containers = [
     diann: [
       version: '2.3.1',
-      singularity: 'oras://ghcr.io/lehtiolab/nfhelaqc:3.2-diann.2.3.1',
+      singularity: 'docker://ghcr.io/lehtiolab/nfhelaqc:3.2-diann.2.3.1',
       docker: 'ghcr.io/lehtiolab/nfhelaqc:3.2-diann.2.3.1',
     ],
     ddamsproteomics: [
